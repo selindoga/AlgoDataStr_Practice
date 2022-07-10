49c84ba14223ef497d23825387fe3565001c2f2d
selindoga <73192627+selindoga@users.noreply.github.com>
2022-07-10 17:04:55 +0300

    flood fill coding solution done

--- src/Flood_Fill/Solution.java
+++ src/Flood_Fill/Solution.java
@@ -1,46 +1,24 @@
 package Flood_Fill;
-
+// 733. Flood Fill
 public class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
-
-
-
-        //if(...){ burdaki ifin içine en kenarlarda ve köşelerde değilse diye yaz
-        // bunun için sc length -1 den küçük olmalı mesela
-            if(image [sr][sc] == image [sr-1][sc]){
-                image [sr-1][sc] = newColor;
-                Find(image, sr-1, sc, newColor);
-
-                // boyq
-                // ve bul (bu da eşitse boya)
-            }
-            if(image [sr][sc] == image [sr+1][sc]){
-                image [sr+1][sc] = newColor;
-                Find(image, sr+1, sc, newColor);
-            }
-            if(image [sr][sc] == image [sr][sc-1]){
-                image [sr][sc-1] = newColor;
-                Find(image, sr, sc-1, newColor);
-            }
-            if(image [sr][sc] == image [sr][sc+1]){
-                image [sr][sc+1] = newColor;
-                Find(image, sr, sc+1, newColor);
-            }
-
+        int colorToBeChanged = image[sr][sc];
+        if (colorToBeChanged != newColor){
+            ChangeColor(image, sr, sc, colorToBeChanged,newColor);
+        }
         return image;
     }
-    void Find(int[][] image_, int sr_, int sc_, int newColor_){
-        if(image_ [sr_][sc_] == image_ [sr_-1][sc_]){
-            image_ [sr_-1][sc_] = newColor_;
-        }
-        if(image_ [sr_][sc_] == image_ [sr_+1][sc_]){
-            image_ [sr_+1][sc_] = newColor_;
-        }
-        if(image_ [sr_][sc_] == image_ [sr_][sc_-1]){
-            image_ [sr_][sc_-1] = newColor_;
-        }
-        if(image_ [sr_][sc_] == image_ [sr_][sc_+1]){
-            image_ [sr_][sc_+1] = newColor_;
+    void ChangeColor(int[][] image, int sr, int sc, int colorToBeChanged, int newColor){
+        if(image[sr][sc] == colorToBeChanged){
+            image[sr][sc] = newColor;
+            if(sr >= 1)
+                ChangeColor(image, sr-1, sc, colorToBeChanged, newColor);
+            if(sc >= 1)
+                ChangeColor(image, sr, sc-1, colorToBeChanged, newColor);
+            if(sr+1 < image.length)
+                ChangeColor(image, sr+1, sc, colorToBeChanged, newColor);
+            if (sc+1 < image[0].length)
+                ChangeColor(image, sr, sc+1, colorToBeChanged, newColor);
         }
     }
 }
